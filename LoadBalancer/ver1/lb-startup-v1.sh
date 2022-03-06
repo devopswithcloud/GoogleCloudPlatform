018daac319cb59c5a2213c74f13d29936d4f2adf
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2022-03-06 21:13:19 +0530

    added raw data

--- LoadBalancer/ver1/lb-startup-v1.sh
+++ LoadBalancer/ver1/lb-startup-v1.sh
@@ -4,7 +4,7 @@ apt-get install -y apache2 php wget
 cd /var/www/html
 rm index.html -f
 rm index.php -f
-wget https://storage.googleapis.com/siva-gcp-public/ver1/index.php
+wget https://raw.githubusercontent.com/devopswithcloud/GoogleCloudPlatform/master/LoadBalancer/ver1/index.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
 sed -i "s|region-here|$REGION|" index.php
