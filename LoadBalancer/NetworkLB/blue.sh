8c9ca261f2c41c44c340f9731c70ac015c45afbc
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2022-03-11 13:46:57 +0530

    added blue.sh

--- LoadBalancer/NetworkLB/blue.sh
+++ LoadBalancer/NetworkLB/blue.sh
@@ -3,10 +3,5 @@ sudo apt-get update
 sudo apt-get install apache2 wget -y
 cd /var/www/html
 rm -rf index.html
-wget https://raw.githubusercontent.com/devopswithcloud/GoogleCloudPlatform/master/LoadBalancer/NetworkLB/blue.html
+wget https://raw.githubusercontent.com/devopswithcloud/GoogleCloudPlatform/master/LoadBalancer/NetworkLB/blue/index.html
 sudo service apache2 restart
-
-
-
-
-
