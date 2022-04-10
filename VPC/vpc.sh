d6e54f4ffe6a15d0e5c4ea13c382706700a81008
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2022-04-10 19:05:26 +0530

    added vpc

--- VPC/vpc.sh
+++ VPC/vpc.sh
@@ -1,56 +1,10 @@
-# These are
-# https://cloud.google.com/sdk/docs/quickstarts
-# https://cloud.google.com/sdk/docs/components
-
-$ lscpu
-$ free -m
-$ lsblk
-$ cat /etc/*release*
-
-$ mkdir /opt/batch10 # this is ephemeral, means it will be gone after the restart of the shell
-
-# Where is cloud shell provisioned ??
-# https://www.google.com/about/datacenters/
+#gcloud commands to create network 
+gcloud compute networks create my-custom-network --subnet-mode=custom
 
 
-gcloud init
-gcloud info
-gcloud version
-gcloud auth list
-gcloud config list
+# To create a subnet to already created network
+gcloud compute networks subnets create subnet-a  --network=my-custom-network --range=10.0.1.0/24 --region=us-central1
+gcloud compute networks subnets create subnet-b --network=my-custom-network --range=10.0.2.0/24 --region=us-central1
 
-
-gcloud auth login
-gcloud auth revoke
-
-gcloud projects list
-
-gcloud compute instances create gcloudinstance
-
-
-
-#https://cloud.google.com/sdk/gcloud/reference/config/configurations/create
-gcloud config list
-gcloud config get-value project
-gcloud config set project <PROJECT_ID>
-gcloud config set compute/zone us-east1
-gcloud config unset compute/zone
-
-gcloud config configurations list
-gcloud config configurations create prod --no-activate
-
-gcloud config list --configuration dev-data
-
-gcloud components list
-
-# Working with API's
-gcloud services -h
-gcloud services list -h
-gcloud services list --available
-gcloud services list --enabled
-gcloud services enable compute.googleapis.com
-gcloud services list --available | grep compute
-
-
-#Get your Cloud project ID by running the following command:
-gcloud config get-value project
+gcloud compute instances create instance-1a --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-a 
+gcloud compute instances create instance-1b --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-a --no-address
