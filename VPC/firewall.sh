5f2795f2fc44cbe2420b30275434dcbb6b40fa4a
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2023-05-04 09:43:21 +0530

    Update firewall.sh

--- VPC/firewall.sh
+++ VPC/firewall.sh
@@ -1,17 +1,27 @@
-### gcloud commands to create a network , subnet and 3 virtual machines 
-* gcloud compute networks create custom-network --subnet-mode custom
+# Create a custom-vpc
+gcloud compute networks create custom-network --subnet-mode=custom
+echo "Network Created Succefsully........"
 
-* gcloud compute networks subnets create subnet-a --network=custom-network --region=us-central1 --range=10.2.1.0/24
+# Create Subnet-a in custom-vpc
+# N/w name, cidr range, region
+gcloud compute networks subnets create subnet-a --network custom-network --region us-central1 --range 10.2.1.0/24
+echo "Subnet-a Created Succesfully......."
 
-* gcloud compute networks subnets create subnet-b --network=custom-network --region=us-central1 --range=10.2.2.0/24
+# Create subnet-b in custom-vpc
+gcloud compute networks subnets create subnet-b --network custom-network --region us-central1 --range 10.2.2.0/24
+echo "Subnet-b Created Succesfully......."
 
-* gcloud compute instances create instance-1a --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-a 
+# Create instance-1a in subnet-a
+gcloud compute instances create instance-1a --zone us-central1-a --subnet=subnet-a --machine-type=f1-micro
 
-* gcloud compute instances create instance-1b --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-a --no-address
+# Create instance-1b in subnet-a
+gcloud compute instances create instance-1b --zone us-central1-a --subnet=subnet-a --machine-type=f1-micro --no-address
 
-* gcloud compute instances create instance-1c --zone=us-central1-a--machine-type=f1-micro --subnet=subnet-a
+# Create instance-1c in subnet-a
+gcloud compute instances create instance-1c --zone us-central1-a --subnet=subnet-a --machine-type=f1-micro
 
-* gcloud compute instances create instance-2 --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-b
+# Create instance-2 in subnet-b
+gcloud compute instances create instance-2 --zone us-central1-a --subnet=subnet-b --machine-type=f1-micro
 
-* gcloud compute instances create instance-3 --zone=us-central1-a --machine-type=f1-micro --subnet=subnet-b --no-address
-
+# Create instance-3 in subnet-b
+gcloud compute instances create instance-3 --zone us-central1-a --subnet=subnet-b --machine-type=f1-micro --no-address
